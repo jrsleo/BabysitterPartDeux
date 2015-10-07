b0615089dbd421b1967dae9b2de2b67b69b4015f
Jordan Schultz <jordan@JordanSchultzs-MacBook-Pro.local>
2015-10-07 15:56:43 -0400

    Refactored magic numbers

--- Babysitter.swift
+++ Babysitter.swift
@@ -3,23 +3,25 @@ import Foundation
 public class BabySitter {
     public func calculatePay (startTime: Int, jobDuration: Int) -> Int {
         let bedTime = 9 // bedtime is assumed to always be 9 PM
+        let midnight = 12
+        let lastHour = 16 // corresponds to 4 am
         var pay = 0
         
         if startTime < bedTime {
             if startTime + jobDuration <= bedTime {
                 pay = calculateDayTimeRate(jobDuration)
             }
-            else if startTime + jobDuration <= 12 {
+            else if startTime + jobDuration <= midnight {
                 pay = calculateDayTimeRate(bedTime - startTime) + calculateNightTimeRate(jobDuration - (bedTime - startTime))
             }
             
         }
-        else if (bedTime <= startTime) && (startTime < 12) {
-            if startTime + jobDuration <= 12 {
+        else if (bedTime <= startTime) && (startTime < midnight) {
+            if startTime + jobDuration <= midnight {
                 pay = calculateNightTimeRate(jobDuration)
             }
-            else if (startTime + jobDuration <= 16) {
-                pay = calculateNightTimeRate(12 - startTime) + calculateEarlyMorningTimeRate(jobDuration - (12 - startTime))
+            else if (startTime + jobDuration <= lastHour) {
+                pay = calculateNightTimeRate(midnight - startTime) + calculateEarlyMorningTimeRate(jobDuration - (midnight - startTime))
             }
         }
         else {
